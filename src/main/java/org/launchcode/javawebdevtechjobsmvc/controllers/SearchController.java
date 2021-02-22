fe58f4d26b58cbb1543af87abc2e939eeda4071a
UshaLaunchcode <usha_ece@yahoo.com>
2021-02-22 17:42:49 -0600

    Finished Assignment #3: Tech Jobs (MVC Edition)

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,7 +25,7 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
-    public String displaySearchResults(Model model, @ModelAttribute String searchType, String searchTerm){
+    public String displaySearchResults(Model model, @RequestParam String searchType, String searchTerm){
          ArrayList<Job> jobs;
         if (searchTerm.toLowerCase().equals("all") || searchTerm == " "){
             jobs = JobData.findAll();
